3276e95e0a6944e43b4c3dd07baca13a996bb704
GeorgePlukov <gplukov@gmail.com>
2017-01-21 17:12:28 -0500

    Stored tested args in vars

--- PA1/pa1.c
+++ PA1/pa1.c
@@ -9,6 +9,9 @@
 // M: number of segments in S
 // c0,c1,c2: letters to be used in property check
 
+// Global thread variables
+int thread_count;
+
 int i = 0;
 int N = 3;
 int L = 1;
@@ -17,7 +20,7 @@ char c0;
 char c1;
 char c2;
 
-
+String
 // A function that determines of a string is numeric
 bool isNumeric(const char *str)
 {
@@ -58,15 +61,21 @@ int main(int argc, char *argv[]) {
         // Input varification
         int correct_lower[4] = {0, 3, 0, 0};
         int correct_upper[4] = {3, 8, 1000, 1000};
+        // Defaults
+        int result[4] = {0,3,1,6};
         for (size_t i = 1; i <= 4; i++) {
                 int curr_arg = atoi (argv[i]);
                 if (!(curr_arg >= correct_lower[i-1] && curr_arg <= correct_upper[i-1])) {
                         printf("[ERROR]: Argument %d must be range %d to %d\n", i, correct_lower[i-1], correct_upper[i-1]);
                         exit(0);
                 }
+                result[i] = curr_arg;
         }
 
-        // if (!(atoi (argv[2]) >= 0 &&  atoi (argv[2]) <= 3)) {
-        //
-        // }
+        // Store integer results into variables
+        i = results[0];
+        N = results[1];
+        L = results[2];
+        M = results[3];
+
 }

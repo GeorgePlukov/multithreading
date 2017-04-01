4907b138fb6755e7fad13a10759447a618f36914
jean-lucas <jeanlpf@hotmail.com>
2017-04-01 13:13:27 -0400

    merge?

---
+++ PA4/PA4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "ppm.h"
+#include "pa4.h"
+
+
+int main(int argc, char** argv) {
+
+	printf("test from pa4.h %d \t from ppm %d \n", a, ppm_get_num());
+
+	return 1;
+}
